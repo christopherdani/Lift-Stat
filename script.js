ade77e470f1a4fba7f92e5342fc37bf5c9c8687e
christopherdani <christopherdani@hotmail.com>
2019-01-14 22:18:25 -0500

    Created forms for the template page

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ function submitDate(){
     console.log($('#datepicker-small').val());
     window.location.href = 'exercises.html';
 }
+
+function addEntry(){
+    var entry = document.getElementById("forms");
+    console.log(document.getElementById("test"));
+    entry.innerHTML+=`
+        <div class="form-row">
+            <div class="col.auto">
+                <input type="text" class="form-control" placeholder="Exercise name">
+            </div>
+            <div class="col.auto">
+                <input type="number" class="form-control" placeholder="Set">
+            </div>
+            <div class="col.auto">
+                <input type="number" class="form-control" placeholder="Rep">
+            </div>
+        </div>`;
+    console.log("test");
+}
