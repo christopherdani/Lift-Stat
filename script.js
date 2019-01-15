3383114d3d4d3075b2fedbf06e609bb68d7f5c3a
christopherdani <christopherdani@hotmail.com>
2019-01-14 22:39:08 -0500

    implemented adding new fields in template.html

--- script.js
+++ script.js
@@ -4,19 +4,16 @@ function submitDate(){
 }
 
 function addEntry(){
-    var entry = document.getElementById("forms");
-    console.log(document.getElementById("test"));
-    entry.innerHTML+=`
-        <div class="form-row">
-            <div class="col.auto">
-                <input type="text" class="form-control" placeholder="Exercise name">
-            </div>
-            <div class="col.auto">
-                <input type="number" class="form-control" placeholder="Set">
-            </div>
-            <div class="col.auto">
-                <input type="number" class="form-control" placeholder="Rep">
-            </div>
-        </div>`;
-    console.log("test");
+    $('.entry').append(`
+    <div class="form-row">
+        <div class="col.auto">
+            <input type="text" class="form-control" placeholder="Exercise name">
+        </div>
+        <div class="col.auto">
+            <input type="number" class="form-control" placeholder="Set">
+        </div>
+        <div class="col.auto">
+            <input type="number" class="form-control" placeholder="Rep">
+        </div>
+    </div>`)
 }
