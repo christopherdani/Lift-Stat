7b17ad7f0c770bb831e75fc74b6b9b50390bc174
christopherdani <christopherdani@hotmail.com>
2019-02-21 21:14:10 -0500

    Now selectively posts sets.
    Didn't realize it, but before, I was only posting the 1st exercise
    progress in session.pug
    
    Encountered a problem where when I switched the button and form id into
    classes, all of them would get posted any time the "save set" button is
    clicked.
    
    Tried having a loop outside of the .click(). But that ended up
    redirecting the page when save set is clicked. Because I think when the
    document is ready, the loop runs and ends before a user can click
    anything. When it is finally clicked, the loop already ended, and
    .click() is never called.
    
    Tried changing it so that all buttons are basically the same. and a loop
    inside of .click(). But this didn't work, because I still run into the
    problem of identifying which button was clicked.
    
    A google search later, I found that I can just find out which button I
    have clicked in e.target.id...
    
    Now I just get the # of the button that was clicked, and pass it into
    the ajax call.
    
    Yay!

--- public/sessionBuilder.js
+++ public/sessionBuilder.js
@@ -5,16 +5,48 @@
     Then POST it when the user is finished with the session and clicks "save session"
 */
 
+// http://james.padolsey.com/javascript/regex-selector-for-jquery/
+jQuery.expr[':'].regex = function(elem, index, match) {
+    var matchParams = match[3].split(','),
+        validLabels = /^(data|css):/,
+        attr = {
+            method: matchParams[0].match(validLabels) ? 
+            matchParams[0].split(':')[0] : 'attr',
+            property: jQuery.trim(matchParams.shift().replace(validLabels,''))
+        },
+        regex = new RegExp(matchParams.join('').replace(/^\s+|\s+$/g,''), 'ig');
+    return regex.test(jQuery(elem)[attr.method](attr.property));
+};
+
+// Update the progress bar.
+function updateProgress(exerciseName){
+    //TODO next time.
+}
+
 $(document).ready(function(){
-    $('#saveSet').click(function(e){
-        
+    // We want to only post the set that we're working on currently. So we need to find out which button was clicked.
+    $('input:regex(id, ^saveSet\\d+)').click(function(e){
+        var selectedSet = e.target.id;
+        var i = -1;
+        // Get the index/id of which set we should post.
+        // length 8 means saveSet/d
+        // length 9 means ssaveSet/d/d
+        if (selectedSet == 8){
+            i = selectedSet.slice(-1);
+        }
+        else {
+            i = selectedSet.slice(-2);
+        }
         e.preventDefault();
+        // Now post it.
         $.ajax({
             url: '/sessionSaveSet',
             type: 'post',
-            data:$('#ajaxSend').serialize(),
-            succes: function() {
-                console.log("Set saved successfuly!");
+            data:$('#ajaxSend' + i).serialize(),
+            dataType: 'json',
+            success: function(){
+                // For some reason, this is not getting called. Not really sure why...
+                console.log('test');
             }
         });
     });
